93343bd32509ebaea16c0238bab926677703aeae
AnessB <bouaoud.aness98@gmail.com>
2021-01-27 12:15:33 +0100

    editefooter

--- pages/components/Layout/components/Footer/Footer.jsx
+++ pages/components/Layout/components/Footer/Footer.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 export default function footer() {
     return (
-	<div className="footer-area-wrapper reveal-footer bg-gray ">
+      
+          	<div className="footer-area-wrapper reveal-footer bg-gray ">
             <div className="footer-area section-space--ptb_80">
                 <div className="container">
                     <div className="row footer-widget-wrapper">
@@ -43,7 +44,38 @@ export default function footer() {
                     </div>
                 </div>
             </div>
-            
+            <div className="footer-copyright-area section-space--pb_30">
+                <div className="container">
+                    <div className="row align-items-center">
+                        <div className="col-md-6 text-center text-md-left">
+                            <span className="copyright-text">&copy; 2020 <a href="https://aftercode.tn">AfterCode</a> All Rights Reserved.</span>
+                        </div>
+                        <div className="col-md-6 text-center text-md-right">
+                            <ul className="list ht-social-networks solid-rounded-icon">
+
+
+                                <li className="item">
+                                    <a href="https://www.facebook.com/Smart.Universities" target="_blank" aria-label="Facebook" className="social-link hint--bounce hint--top hint--primary">
+                                        <i className="fab fa-facebook-f link-icon"></i>
+                                    </a>
+                                </li>
+                                <li className="item">
+                                    <a href="https://www.instagram.com/mediterranean_institut_tunisia" target="_blank" aria-label="Instagram" className="social-link hint--bounce hint--top hint--primary">
+                                        <i className="fab fa-instagram link-icon"></i>
+                                    </a>
+                                </li>
+                                <li className="item">
+                                    <a href="https://linkedin.com/" target="_blank" aria-label="Linkedin" className="social-link hint--bounce hint--top hint--primary">
+                                        <i className="fab fa-linkedin link-icon"></i>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
         </div>
-    )
+        </div>
+  
+ 
+  )
 }
