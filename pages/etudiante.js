a7b80e1221eca38fb1b087797b3a6ef860df2a42
AnessB <bouaoud.aness98@gmail.com>
2021-01-27 12:13:09 +0100

    etudiante

---
+++ pages/etudiante.js
@@ -0,0 +1,184 @@
+import React from 'react'
+
+export default function etudiante() {
+    return (
+        <>
+<div className="breadcrumb-area">
+    <div className="container">
+        <div className="row">
+            <div className="col-12">
+                <div className="breadcrumb_box text-center">
+                    <h2 className="breadcrumb-title ">VIE ÉSTUDIANTE</h2>
+                  
+                    <ul className="breadcrumb-list">
+                        <li className="breadcrumb-item"><a href="formations">La vie à l'université</a></li>
+                        <li className="breadcrumb-item active">Vie etudiante</li>
+                    </ul>
+             
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div id="main-wrapper">
+    <div className="site-wrapper-reveal">
+        <div className="blog-pages-wrapper section-space--ptb_100">
+            <div className="container">
+                <div className="row">
+
+                    <div className="col-lg-12 order-lg-2 order-1">
+
+                        <div className="main-blog-wrap">
+          
+                            <div className="single-blog-item post-list-wrapper xs-list-blog-item border-bottom wow move-up">
+                                <div className="row ">
+                                    <div className="col-lg-6">
+                                        <blockquote>
+                                            <p className="p1">Learning center </p>
+                                        </blockquote>
+                                        <p style={{textAlign:"justify"}}>
+                                            Agrandi et rénové en 2017 au Rez de chaussée, le Learning Center est bien
+                                            plus qu’une bibliothèque. Sur place, des milliers d’ouvrages et de E-books,
+                                            journaux internationaux, revues académiques et ressources multimédia sont
+                                            accessibles aux étudiants, sous forme de CD, clés USB, dans un cadre
+                                            agréable, doté d’espaces de travail individuels et de salles de travail
+                                            collaboratif, avec partout un accès Wi-Fi et des PC en libre-service.
+                                        </p>
+                                        <p style={{textAlign:"justify"}}>
+                                            Le Learning Center fonctionne également comme un portail documentaire en
+                                            ligne, offrant des milliers de ressources (e-books, article...) consultable
+                                            à distances, (en cas de besoin) ainsi que des services de recherche et de
+                                            réservation d’ouvrage.Tous les programmes disposent en outre d'un espace «
+                                            e-Learning » accessible depuis Internet.
+                                        </p>
+                                    </div>
+
+                                    <div className="col-lg-6">
+                                        <div className="">
+                                            <img src="assets/images/blog/e-learning.jpg"
+                                                alt="certifications.jpg" width="600px" className=""/>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                         
+
+                 
+                            <div className="single-blog-item post-list-wrapper xs-list-blog-item border-bottom wow move-up">
+                                <div className="row ">
+                                    <div className="col-lg-6">
+                                   
+                                        <blockquote>
+                                            <p className="p1">Bibliothéque </p>
+                                        </blockquote>
+                                        <p style={{textAlign:"justify"}}>
+                                            La bibliothèque est équipée d’outils numériques et accès Wifi. Les
+                                            ordinateurs sont équipés d’applications choisies par la bibliothèque :
+                                            bibliothèque, presse écrite, audiovisuel, ressources en ligne ainsi que de
+                                            logiciels bureautique. La bibliothèque est équipée aussi d’un photocopieur
+                                            en libre accès. Un responsable est à votre service pour acquisitions des
+                                            livres, périodiques et bases de données.
+                                        </p>
+
+                                  
+                                    </div>
+                                    <div className="col-lg-6">
+                                        <blockquote>
+                                            <p className="p1"> Espace SMART de Co-working </p>
+                                        </blockquote>
+                                        <p>
+                                        <p style={{textAlign:"justify"}}>
+                                            L’université dispose de son First Co-working smart place : Salle de
+                                            Co-working sur ses 4 sites. <br/>
+                                            Bienvenue dans la Famille Méditerranéenne
+                                        </p>
+                                        <p style={{textAlign:"justify"}}>
+                                        <ul>
+                                            <li> <b> CIO</b> : Centre Information et Orientation </li>
+                                            <li> <b> Cci </b>: Service Social et Service Financier</li>
+                                        </ul>
+                                        </p>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+               
+                            <div className="">
+                                <div className="row ">
+                                    <div className="col-lg-6">
+                              
+                                        <blockquote>
+                                            <p className="p1"> Une université accessible d’un simple clic </p>
+                                        </blockquote>
+                                        <p style={{textAlign:"justify"}}>
+                                            Accessible depuis Internet grâce à un mot de passe personnel, l’université
+                                            virtuelle concerne la majorité des programmes et des formations diplômantes
+                                            du Groupe Méditerranéen.
+                                        </p>
+                                        <p style={{textAlign:"justify"}}>Depuis votre domicile ou n’importe quel ordinateur
+                                            dans le monde, un portail pédagogique en ligne. <b>www.umlt.ens.tn </b>vous
+                                            donnez accès à toutes les informations et outils indispensables à votre
+                                            formation, parmi lesquels vous trouverez des plans de cours, des documents
+                                            téléchargeables, des vidéos ou des forums de discussion. C’est aussi un
+                                            espace de réactivité où vous pourrez participer en postant un fichier
+                                            important, par exemple.
+                                            Depuis septembre 2017, MyUMLT Apps, basé sur la technologie Web, est la
+                                            nouvelle plateforme de communication et de travail collaboratif partagée par
+                                            toute la communauté de l'MIT.</p>
+
+                        
+                                    </div>
+                                    <div className="col-lg-6">
+                                        <blockquote>
+                                            <p className="p1"> Espace de restauration et Loisirs </p>
+                                        </blockquote>
+                                        <p style={{textAlign:"justify"}}>
+                                            Un Restaurant est disponible dans les locaux de l’université au pole
+                                            technologique à Tunis.
+                                        </p>
+                                        <p style={{textAlign:"justify"}}>Des accords avec des Restaurants et des salles de
+                                            sports, Piscine, et loisirs sont signés par l’Université à Tunis et à
+                                            Nabeul.
+                                            L’Université facilite également l’intégration, l’insertion, la procédure de
+                                            Visa et l’acheminement de l’Aéroport vers les locaux.</p>
+
+                                        <p> Aussi Il existe de nombreux lieux de restauration : un Café -restaurant très
+                                            fréquenté situé dans un quartier d’affaires avec un large choix d’entrées,
+                                            de plats et de desserts à proximité du bâtiment de l’école. On peut se
+                                            rendre aussi dans d’autres cafétérias et restaurants à proximité de l’école.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+
+
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="cta-image-area_one section-space--ptb_80 cta-bg-image_one">
+            <div className="container">
+                <div className="row align-items-center">
+                    <div className="col-xl-8 col-lg-7">
+                        <div className="cta-content md-text-center">
+                            <h3 className="heading text-white">Bourses d'études et places gratuites et accés dés la
+                                Terminale à nos modules de E-learning et sur place </h3>
+                        </div>
+                    </div>
+                    <div className="col-xl-4 col-lg-5">
+                        <div className="cta-button-group--one text-center">
+
+                            <a href="contact" className="btn btn--secondary  btn-two"><span className="btn-icon mr-2"><i
+                                        className="far fa-info-circle"></i></span> Contact</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</div>
+    </div>
+        </>
+    )
+}
