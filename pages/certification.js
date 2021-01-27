1d8c36e626bc382459ea4f45cfc10f7e6428ac6b
AnessB <bouaoud.aness98@gmail.com>
2021-01-27 12:12:36 +0100

    certification

---
+++ pages/certification.js
@@ -0,0 +1,330 @@
+import React from 'react'
+
+export default function certification() {
+    return (
+        <>
+<div className="breadcrumb-area">
+    <div className="container">
+        <div className="row">
+            <div className="col-12">
+                <div className="breadcrumb_box text-center">
+                    <h2 className="breadcrumb-title ">CENTRE DE CERTIFICATIONS ET DE FORMATION</h2>
+               
+                    <ul className="breadcrumb-list">
+                        <li className="breadcrumb-item"><a href="">la Vie à l'université</a></li>
+                        <li className="breadcrumb-item active">Certifications</li>
+                    </ul>
+ 
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="main-wrapper">
+    <div className="site-wrapper-reveal">
+
+
+        <div className="feature-images-wrapper position-relative">
+            <div className="grid-wrapper">
+                <div className="line line-1"></div>
+                <div className="line line-2"></div>
+                <div className="line line-3"></div>
+                <div className="line line-4"></div>
+            </div>
+            <div className="swiper-container service-slider__container">
+                <div className="swiper-wrapper service-slider__wrapper feature-images__three">
+                    <div className="swiper-slide">
+      
+                        <a href="#" className="ht-box-images style-03 wow move-up">
+                            <div className="image-box-wrap">
+                                <div className="box-image">
+                                    <img className="img-fulid"
+                                        src="assets/images/icons/mitech-home-resolutions-box-image-02-100x98.png"
+                                        alt=""/>
+                                </div>
+                                <div className="content">
+                                    <h6 className="heading">Des conférences internationales </h6>
+                                    <div className="text"> Présence de prix Nobels : James Hekman, Harry Markovitz, E.
+                                        Prescott,Myron Scholes, etc.
+                                        Les étudiants et enseignants d’UM participent et présentent des articles.</div>
+                                </div>
+                            </div>
+                        </a>
+               
+                    </div>
+                    <div className="swiper-slide">
+           
+                        <a href="#" className="ht-box-images style-03 wow move-up">
+                            <div className="image-box-wrap">
+                                <div className="box-image">
+                                    <img className="img-fulid" width="151px" height="115px"
+                                        src="assets/images/tunis.jpg" alt=""/>
+                                </div>
+                                <div className="content">
+                                    <h5 className="heading">Diplôme agrée par l’État </h5>
+                                    <div className="text">“L’université offre des diplômes agrées par l’État , des stages en
+                                        Tunisie et à l’étranger.”</div>
+                                </div>
+                            </div>
+                        </a>
+                   
+                    </div>
+                    <div className="swiper-slide">
+    
+                        <a href="#" className="ht-box-images style-03 wow move-up">
+                            <div className="image-box-wrap">
+                                <div className="box-image">
+                                    <img className="img-fulid" src="assets/images/france.png" alt=""/>
+                                </div>
+
+                                <div className="content">
+                                    <h6 className="heading">Plusieurs diplômes internationaux </h6>
+                                    <div className="text">“ Inscrirez-vous au diplôme tunisien et bénéficier d'un diplôme
+                                        français public avec nos partenaires.”</div>
+                                </div>
+                            </div>
+                        </a>
+                       
+                    </div>
+                    <div className="swiper-slide">
+           
+                        <a href="#" className="ht-box-images style-03 wow move-up">
+                            <div className="image-box-wrap">
+                                <div className="box-image">
+                                    <img className="img-fulid"
+                                        src="assets/images/icons/mitech-processing-service-image-04-80x83.png"
+                                        alt=""/>
+                                </div>
+                                <div className="content">
+                                    <h5 className="heading">First smart univerty in africa</h5>
+                                    <div className="text">“ BLP dispose d’une plateforme d’enseignement à distance et de
+                                        conseillers pédagogiques couvrant tous les programmes .”</div>
+                                </div>
+                            </div>
+                        </a>
+                   
+                    </div>
+                    <div className="swiper-slide">
+                
+                        <a href="#" className="ht-box-images style-03 wow move-up">
+                            <div className="image-box-wrap">
+                                <div className="box-image">
+                                    <img className="img-fulid" width="151px" height="115px"
+                                        src="assets/images/safa.jpg" alt=""/>
+                                </div>
+                                <div className="content">
+                                    <h5 className="heading">Des projets à l’international</h5>
+                                    <div className="text">“La mise en place d’un référentiel de compétences et l’interaction
+                                        entre le monde académique et professionnel .”</div>
+                                </div>
+                            </div>
+                        </a>
+                
+                    </div>
+
+                    <div className="swiper-slide">
+                
+                        <a href="#" className="ht-box-images style-03 wow move-up">
+                            <div className="image-box-wrap">
+                                <div className="box-image">
+                                    <img className="img-fulid"
+                                        src="assets/images/icons/mitech-processing-service-image-04-80x83.png"
+                                        alt=""/>
+                                </div>
+                                <div className="content">
+                                    <h5 className="heading">Certifications</h5>
+                                    <div className="text">Élargissez les perspectives de votre carrières grâce à des
+                                        certificats reconnus mondialement.</div>
+                                </div>
+                            </div>
+                        </a>
+                  
+                    </div>
+
+
+                </div>
+                <div className="swiper-pagination swiper-pagination-service section-space--mt_30"></div>
+            </div>
+        </div>
+       
+
+        <div className="our-company-history section-space--ptb_100">
+            <div className="container-fluid">
+                <div className="grid-wrapper">
+                    <div className="line line-1"></div>
+                    <div className="line line-2"></div>
+                    <div className="line line-3"></div>
+                    <div className="line line-4"></div>
+                </div>
+
+                <div className="row align-items-center">
+                    <div className="col-lg-6">
+                        <div className="faq-custom-col">
+                            <div className="section-title-wrap">
+
+                                <h3 className="heading">Certifications </h3>
+
+                                <p className="text mt-30">BLP intègre les certifications dans le cursus universitaire.
+                                    Chaque étudiant peut choisir de valoriser les compétences acquises par son
+                                    investissement dans des activités complémentaires qui donnent lieu à des
+                                    certifications. BLP en partenariat avec des professionnels et des organismes
+                                    extérieurs (centre AMIDEAST, ISET Rades..), a élaboré les programmes de ces
+                                    certifications .</p>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-6">
+                        <div className="ht-banner-01 ">
+                            <img className="img-fluid border-radus-5 animation_images one wow fadeInDown"
+                                src="assets/images/radio1.jpg" alt=""/>
+                        </div>
+                        <div className="ht-banner-02">
+                        </div>
+                        <div className="col-lg-6 col-md-6 mt-30">
+                            <div className="">
+                                <img src="assets/images/certifications.jpg" alt="certifications.jpg"
+                                    className=""/>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+    
+        <div className="gradation-process-area bg-gray section-space--ptb_100">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-12">
+                        <div className="ht-gradation style-01">
+                     
+                            <div className="item item-1 animate  wow fadeInRight" data-wow-delay="0.1s">
+                                <div className="line"></div>
+                                <div className="circle-wrap">
+                                    <div className="mask">
+                                        <div className="wave-pulse wave-pulse-1"></div>
+                                        <div className="wave-pulse wave-pulse-2"></div>
+                                        <div className="wave-pulse wave-pulse-3"></div>
+                                    </div>
+
+                                    <h6 className="circle">1</h6>
+                                </div>
+
+                                <div className="content-wrap">
+
+                                    <h6 className="heading">01. Certifications SOLIDWORKS</h6>
+
+                                    <div className="text">Elles permettent d'évaluer précisément vos connaissances et
+                                        savoir-faire avec les logiciels SOLIDWORKS.</div>
+
+
+                                </div>
+                            </div>
+                          
+                            <div className="item item-1 animate  wow fadeInRight" data-wow-delay="0.15s">
+                                <div className="line"></div>
+                                <div className="circle-wrap">
+                                    <div className="mask">
+                                        <div className="wave-pulse wave-pulse-1"></div>
+                                        <div className="wave-pulse wave-pulse-2"></div>
+                                        <div className="wave-pulse wave-pulse-3"></div>
+                                    </div>
+
+                                    <h6 className="circle">2</h6>
+                                </div>
+
+                                <div className="content-wrap">
+
+                                    <h6 className="heading">02. Certification linguistique</h6>
+
+                                    <div className="text">Les tests Test of English for International Communication (TOEIC)
+                                        sont des certifications standardisées <br/> permettant d'évaluer le niveau
+                                        d'anglais des locuteurs non anglophones tant au niveau de l'écrit que de l'oral.
+                                    </div>
+
+                                </div>
+                            </div>
+                         
+                            <div className="item item-1 animate  wow fadeInRight" data-wow-delay="0.20s">
+                                <div className="line"></div>
+                                <div className="circle-wrap">
+                                    <div className="mask">
+                                        <div className="wave-pulse wave-pulse-1"></div>
+                                        <div className="wave-pulse wave-pulse-2"></div>
+                                        <div className="wave-pulse wave-pulse-3"></div>
+                                    </div>
+
+                                    <h6 className="circle">3</h6>
+                                </div>
+
+                                <div className="content-wrap">
+
+                                    <h6 className="heading">03. Certifications Cisco, Microsoft, Linux</h6>
+
+                                    <div className="text">BLP permet à tous les techniciens et ingénieurs de préparer <br/>
+                                        leur certification Cisco - CCNA, CCNP, CCIE - en suivant une formation
+                                        officielle Cisco.</div>
+                                </div>
+                            </div>
+                    
+                            <div className="item item-1 animate wow fadeInRight" data-wow-delay="0.25s">
+                                <div className="line"></div>
+                                <div className="circle-wrap">
+                                    <div className="mask">
+                                        <div className="wave-pulse wave-pulse-1"></div>
+                                        <div className="wave-pulse wave-pulse-2"></div>
+                                        <div className="wave-pulse wave-pulse-3"></div>
+                                    </div>
+
+                                    <h6 className="circle">4</h6>
+                                </div>
+
+                                <div className="content-wrap">
+
+                                    <h6 className="heading">04. Certification Oracle</h6>
+
+                                    <div className="text">C'est le leader mondial des bases de données. <br/> Toute personne
+                                        visant un poste d’administrateur où développeur de base de données doit être
+                                        capable d’installer, <br/> configurer, administrer, sécuriser et optimiser une
+                                        base de données. D’autre part il doit être capable de concevoir et développer
+                                        toutes sortes de requêtes.</div>
+
+
+                                </div>
+                            </div>
+                   
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    
+        <div className="cta-image-area_one section-space--ptb_80 cta-bg-image_one">
+            <div className="container">
+                <div className="row align-items-center">
+                    <div className="col-xl-8 col-lg-7">
+                        <div className="cta-content md-text-center">
+                            <h3 className="heading text-white">Bourses d'études et places gratuites et accés dés la
+                                Terminale à nos modules de E-learning et sur place </h3>
+                        </div>
+                    </div>
+                    <div className="col-xl-4 col-lg-5">
+                        <div className="cta-button-group--one text-center">
+
+                            <a href="contact" className="btn btn--secondary  btn-two"><span className="btn-icon mr-2"><i
+                                        className="far fa-info-circle"></i></span> Contact</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    </div>
+
+        </>
+    )
+}
