487537cfe9f5a2538e9e9f4cbb54f0d02056c625
AnessB <bouaoud.aness98@gmail.com>
2021-01-27 12:11:26 +0100

    associative

---
+++ pages/associative.js
@@ -0,0 +1,131 @@
+import React from 'react'
+
+export default function associative() {
+    return (
+        <>
+<div className="breadcrumb-area">
+    <div className="container">
+        <div className="row">
+            <div className="col-12">
+                <div className="breadcrumb_box text-center">
+                    <h2 className="breadcrumb-title ">LA VIE ASSOCIATIVE</h2>
+                
+                    <ul className="breadcrumb-list">
+                        <li className="breadcrumb-item"><a href="">La vie à l'université</a></li>
+                        <li className="breadcrumb-item active">La vie associative</li>
+                    </ul>
+              
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="main-wrapper">
+    <div className="site-wrapper-reveal">
+ 
+        <div className="blog-pages-wrapper section-space--pt_100 section-space--pb_70">
+            <div className="container masonry-activation">
+                <h3 className="post-title text-center">
+                    La vie associative
+                </h3>
+                <br/><br/>
+                <div className="row">
+                    <div className="col-lg-6 ">
+                        <p>La Vie Associative est un lieu d'échange, de partage, de réflexion et de
+                            formation à destination des associations.
+                            Créées avec les responsables de la vie associative et universitaire du
+                            Groupe Méditerranéen, les associations sont une composante de
+                            l'Université. Elles permettent de partager les expériences dans un cadre
+                            de réflexion et de formation. Ce sont des activités, et un lieu ouverts à
+                            tous les étudiants. <br/> Les formations associent enseignants et étudiants :
+                            social, culture, sport, loisirs, environnement, patrimoine, droits de
+                            l'homme, consommation, etc... <br/>Un comité de pilotage composé de
+                            représentants des groupes définit les orientations à prendre et permet aux
+                            associations de rester au cœur des préoccupations du Groupe
+                            Méditerranéen.</p>
+                    </div>
+                    <div className="col-lg-6 ">
+                        <video controls>
+                            <source src="/assets/video/mit1.mp4 " type="video/mp4" frameborder="0"
+                                allowfullscreen/>
+                            Votre navigateur ne supporte pas la balise vidéo.
+                        </video>
+                    </div>
+                </div>
+
+                <blockquote>
+                    <p className="p1">Club Sportive </p>
+                </blockquote>
+                
+                <div className="gallery-wrapper section-space--pt_80 ">
+                    <div className="row">
+                        <div className="col-lg-12">
+                        
+                            <div className="gallery-warp popup-images ">
+                            
+                                <a className="single-gallery gallery-grid wow move-up"
+                                    href="assets/images/blog/club-sportive.jpg">
+                                    <div className="single-gallery__item">
+                                        <div className="single-gallery__thum bg-item-images bg-img"
+                                            data-bg="assets/images/blog/club-sportive.jpg">
+
+                                        </div>
+                                        <div className="ht-overlay"></div>
+                                        <div className="overlay-icon">
+                                            <i className="far fa-search"></i>
+                                        </div>
+                                    </div>
+                                </a>
+                              
+                                <a className="single-gallery gallery-grid wow move-up"
+                                    href="assets/images/blog/gymanstique-blp.jpg">
+                                    <div className="single-gallery__item">
+                                        <div className="single-gallery__thum bg-item-images bg-img"
+                                            data-bg="assets/images/blog/gymanstique-blp.jpg"></div>
+                                        <div className="ht-overlay"></div>
+                                        <div className="overlay-icon">
+                                            <i className="far fa-search"></i>
+                                        </div>
+                                    </div>
+                                </a>
+                              
+                                <a className="single-gallery gallery-grid wow move-up"
+                                    href="assets/images/blog/fitness-blp.jpg">
+                                    <div className="single-gallery__item">
+                                        <div className="single-gallery__thum bg-item-images bg-img"
+                                            data-bg="assets/images/blog/fitness-blp.jpg"></div>
+                                        <div className="ht-overlay"></div>
+                                        <div className="overlay-icon">
+                                            <i className="far fa-search"></i>
+                                        </div>
+                                    </div>
+                                </a>
+                             
+                                <a className="single-gallery gallery-grid wow move-up"
+                                    href="assets/images/blog/sport-blp.jpg">
+                                    <div className="single-gallery__item">
+                                        <div className="single-gallery__thum bg-item-images bg-img"
+                                            data-bg="assets/images/blog/sport-blp.jpg"></div>
+                                        <div className="ht-overlay"></div>
+                                        <div className="overlay-icon">
+                                            <i className="far fa-search"></i>
+                                        </div>
+                                    </div>
+                                </a>
+                               
+                            </div>
+                      
+                        </div>
+                    </div>
+                </div>
+       
+            </div>
+        </div>
+  
+
+    </div>
+    </div>
+        </>
+    )
+}
