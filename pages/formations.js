ec18cca61fb81a08f6e3da292ed7d5473c0ebc16
AnessB <bouaoud.aness98@gmail.com>
2021-01-27 12:13:25 +0100

    formation

---
+++ pages/formations.js
@@ -0,0 +1,312 @@
+import React from 'react'
+
+export default function formations() {
+    return (
+        <>
+    <div className="breadcrumb-area">
+        <div className="container">
+            <div className="row">
+                <div className="col-12">
+                    <div className="breadcrumb_box text-center">
+                        <h2 className="breadcrumb-title text-color-primary">FORMATIONS</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  
+    <div id="main-wrapper">
+        <div className="site-wrapper-reveal">
+
+                 <div className="feature-images-wrapper position-relative">
+                <div className="grid-wrapper">
+                    <div className="line line-1"></div>
+                    <div className="line line-2"></div>
+                    <div className="line line-3"></div>
+                    <div className="line line-4"></div>
+                </div>
+                <div className="swiper-container service-slider__container">
+                    <div className="swiper-wrapper service-slider__wrapper feature-images__three">
+                    <div className="swiper-slide">
+              
+                            <a href="#" className="ht-box-images style-03 wow move-up">
+                                <div className="image-box-wrap">
+                                    <div className="box-image">
+                                        <img className="img-fulid" src="assets/images/icons/mitech-home-resolutions-box-image-02-100x98.png" alt=""/>
+                                    </div>
+                                    <div className="content">
+                                        <h6 className="heading">Des conférences internationales </h6>
+                                        <div className="text"> Présence de prix Nobels : James Hekman, Harry Markovitz, E. Prescott,Myron Scholes, etc.
+                                        Les étudiants et enseignants d’UM participent et présentent des articles.</div>
+                                    </div>
+                                </div>
+                            </a>
+      
+                        </div>
+                        <div className="swiper-slide">
+       
+                            <a href="#" className="ht-box-images style-03 wow move-up">
+                                <div className="image-box-wrap">
+                                    <div className="box-image">
+                                        <img className="img-fulid" src="assets/images/tunis.jpg" alt=""/>
+                                    </div>
+                                    <div className="content">
+                                        <h5 className="heading">Diplôme agrée par l’État </h5>
+                                        <div className="text">“L’université offre des diplômes agrées par l’État , des stages en Tunisie et à l’étranger.”</div>
+                                    </div>
+                                </div>
+                            </a>
+          
+                        </div>
+                        <div className="swiper-slide">
+                   
+                            <a href="#" className="ht-box-images style-03 wow move-up">
+                                <div className="image-box-wrap">
+                                    <div className="box-image">
+                                        <img className="img-fulid" src="assets/images/france.png" alt=""/>
+                                    </div>
+                                 
+                                    <div className="content">
+                                        <h5 className="heading">Plusieurs diplômes internationaux  </h5>
+                                        <div className="text">“ Inscrirez-vous au diplôme tunisien et bénéficier d'un diplôme français public avec nos partenaires.”</div>
+                                    </div>
+                                </div>
+                            </a>
+                      
+                        </div>
+                        <div className="swiper-slide">
+                      
+                            <a href="#" className="ht-box-images style-03 wow move-up">
+                                <div className="image-box-wrap">
+                                    <div className="box-image">
+                                        <img className="img-fulid" src="assets/images/icons/mitech-processing-service-image-04-80x83.png" alt=""/>
+                                    </div>
+                                    <div className="content">
+                                        <h5 className="heading">First smart univerty in africa</h5>
+                                        <div className="text">“ BLP dispose d’une plateforme d’enseignement à distance et de
+                                       conseillers pédagogiques couvrant tous les programmes .”</div>
+                                    </div>
+                                </div>
+                            </a>
+                        
+                        </div>
+                        <div className="swiper-slide">
+                     
+                            <a href="#" className="ht-box-images style-03 wow move-up">
+                                <div className="image-box-wrap">
+                                    <div className="box-image">
+                                        <img className="img-fulid" src="assets/images/safa.jpg" alt=""/>
+                                    </div>
+                                    <div className="content">
+                                        <h5 className="heading">Des projets  à l’international</h5>
+                                        <div className="text">“La mise en place d’un référentiel de compétences et  l’interaction entre le monde académique et professionnel .”</div>
+                                    </div>
+                                </div>
+                            </a>
+                         
+                        </div>
+                        
+                        <div className="swiper-slide">
+                         
+                            <a href="#" className="ht-box-images style-03 wow move-up">
+                                <div className="image-box-wrap">
+                                    <div className="box-image">
+                                        <img className="img-fulid" src="assets/images/icons/mitech-processing-service-image-04-80x83.png" alt=""/>
+                                    </div>
+                                    <div className="content">
+                                        <h5 className="heading">Certifications</h5>
+                                        <div className="text">Élargissez les perspectives de votre carrières grâce à des certificats reconnus mondialement.</div>
+                                    </div>
+                                </div>
+                            </a>
+                          
+                        </div>
+                        
+                      
+                    </div>
+                
+            <div className="fun-fact-wrapper bg-theme-default section-space--pb_30 section-space--pt_60">
+                <div className="container">
+                    <div className="computing-info-box text-center ">
+                        
+                        <h4 style={{color: "white"}} >“ La formation dispensée par notre école est nécessaire pour savoir s'adapter aux mutations profondes de l'économie internationale. ”</h4>
+                        <div className="contact-us-button mt-20">
+                            <a href="/inscription" className="btn btn--secondary">Pré-inscription</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+                </div>
+            </div>
+        
+            <div className="projects-wrapper section-space--pb_70 section-space--pt_100 projects-masonary-wrapper">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-12">
+                            <div className="section-title-wrap text-center section-space--mb_40">
+                                <h3 className="heading">Bénéficiez par <span className="text-color-primary"> nos formations</span></h3>
+                            </div>
+
+                            <div className="messonry-button text-center  section-space--mb_60">
+                                <button data-filter="*" className="is-checked"><span className="filter-text">Tout</span><span className="filter-counter">8</span></button>
+                                <button data-filter=".cat--1"><span className="filter-text">Licences fondamentales</span> <span className="filter-counter">3</span></button>
+                                <button data-filter=".cat--2"><span className="filter-text">Mastères professionnels</span> <span className="filter-counter">5</span></button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="projects-case-wrap">
+                        <div className="row mesonry-list">
+
+                     
+                            <div className="col-lg-4 col-md-6 cat--2">
+                       
+                                <a href="mrid" className="projects-wrap style-2">
+                                    <div className="projects-image-box">
+                                        <div className="projects-image">
+                                            <img className="img-fluid" src="assets/images/projects/inter.jpg" alt=""/>
+                                        </div>
+                                        <div className="content">
+                                            <div className="post-categories">Mastère professionnel en sciences politiques </div>
+                                            <h6 className="heading">Relations internationales et diplomatie</h6>
+                                        </div>
+                                    </div>
+                                </a>
+                         
+                            </div>
+
+                            <div className="col-lg-4 col-md-6 cat--2 ">
+                       
+                                <a href="mdp" className="projects-wrap style-2">
+                                    <div className="projects-image-box">
+                                        <div className="projects-image">
+                                            <img className="img-fluid" src="assets/images/projects/droit.jpg" alt=""/>
+                                        </div>
+                                        <div className="content">
+                                            <div className="post-categories">Mastère professionnel en Droit </div>
+                                            <h6 className="heading">Droit privé des affaires</h6>
+                                        </div>
+                                    </div>
+                                </a>
+                         
+                            </div>
+
+                            <div className="col-lg-4 col-md-6 cat--2">
+                          
+                                <a href="mire" className="projects-wrap style-2">
+                                    <div className="projects-image-box">
+                                        <div className="projects-image">
+                                            <img className="img-fluid" src="assets/images/projects/finance.jpg" alt=""/>
+                                        </div>
+                                        <div className="content">
+                                            <div className="post-categories">Mastère professionnel en sciences Economiques </div>
+                                            <h6 className="heading">Ingénierie des risques Economiques et financiers</h6>
+                                        </div>
+                                    </div>
+                                </a>
+                          
+                            </div>
+                            <div className="col-lg-4 col-md-6 cat--2">
+                   
+                                <a href="massurancebanque" className="projects-wrap style-2">
+                                    <div className="projects-image-box">
+                                        <div className="projects-image">
+                                            <img className="img-fluid" src="assets/images/projects/gestion.jpg" alt=""/>
+                                        </div>
+                                        <div className="content">
+                                            <div className="post-categories">Mastère professionnel en sciences Economiques</div>
+                                            <h6 className="heading">Assurance, banque, finance</h6>
+                                        </div>
+                                    </div>
+                                </a>
+        
+                            </div>
+                            <div className="col-lg-4 col-md-6 cat--2">
+              
+                                <a href="mmarchefinancier" className="projects-wrap style-2">
+                                    <div className="projects-image-box">
+                                        <div className="projects-image">
+                                            <img className="img-fluid" src="assets/images/projects/risque.jpg" alt=""/>
+                                        </div>
+                                        <div className="content">
+                                            <div className="post-categories">Mastère professionnel  en Sciences de gestion </div>
+                                            <h6 className="heading"> Marchés financiers et gestion des risques</h6>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+
+                            <div className="col-lg-4 col-md-6 cat--1">
+                 
+                                <a href="ldroitpublic" className="projects-wrap style-2">
+                                    <div className="projects-image-box">
+                                        <div className="projects-image">
+                                            <img className="img-fluid" src="assets/images/projects/droit2.jpg" alt=""/>
+                                        </div>
+                                        <div className="content">
+                                            <div className="post-categories">Licence fondamentales en Droit </div>
+                                            <h6 className="heading"> Droit Public</h6>
+                                        </div>
+                                    </div>
+                                </a>
+            
+                            </div>
+                            <div className="col-lg-4 col-md-6 cat--1">
+                 
+                                <a href="ldroitprive" className="projects-wrap style-2">
+                                    <div className="projects-image-box">
+                                        <div className="projects-image">
+                                            <img className="img-fluid" src="assets/images/projects/droit3.jpg" alt="licence-droit-prive-blp"/>
+                                        </div>
+                                        <div className="content">
+                                            <div className="post-categories">Licence fondamentales en Droit</div>
+                                            <h6 className="heading"> Droit Privé</h6>
+                                        </div>
+                                    </div>
+                                </a>
+   
+                            </div>
+                            <div className="col-lg-4 col-md-6 cat--1">
+                   
+                                <a href="marketing" className="projects-wrap style-2">
+                                    <div className="projects-image-box">
+                                        <div className="projects-image">
+                                            <img className="img-fluid" src="assets/images/projects/marketing.jpg" alt="licence-marketing-blp"/>
+                                        </div>
+                                        <div className="content">
+                                            <div className="post-categories">Licence fondamentales en Sciences de gestion</div>
+                                            <h6 className="heading"> Marketing</h6>
+                                        </div>
+                                    </div>
+                                </a>
+                       
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="cta-image-area_one section-space--ptb_80 cta-bg-image_one">
+                <div className="container">
+                    <div className="row align-items-center">
+                        <div className="col-xl-8 col-lg-7">
+                            <div className="cta-content md-text-center">
+                                <h3 className="heading text-white">First smart Co-working place  <span className="text-color-secondary"> in AFRICA</span></h3>
+                            </div>
+                        </div>
+                        <div className="col-xl-4 col-lg-5">
+                            <div className="cta-button-group--one text-center">
+                            
+                                <a href="contact" className="btn btn--secondary  btn-two"><span className="btn-icon mr-2"><i className="far fa-info-circle"></i></span>Contact</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+</div>
+        </div>
+        </>
+    )
+}
