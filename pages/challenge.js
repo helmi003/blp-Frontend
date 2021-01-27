98435430f62f75c6cf4c78fee5e2b5c93e39d46d
AnessB <bouaoud.aness98@gmail.com>
2021-01-27 12:12:47 +0100

    challange

--- pages/challenge.js
+++ pages/challenge.js
@@ -3,6 +3,166 @@ import React from 'react'
 export default function challenge() {
     return (
         <>
+    <div className="breadcrumb-area">
+        <div classNameName="container">
+            <div className="row">
+                <div className="col-12">
+                    <div className="breadcrumb_box text-center">
+                        <h2 className="breadcrumb-title ">LES NUITS DE CHALLENGE</h2>
+                      
+                        <ul className="breadcrumb-list">
+                            <li className="breadcrumb-item"><a href="accueil">BLP en digital</a></li>
+                            <li className="breadcrumb-item active">Les nuits de challenge</li>
+                        </ul>
+                     
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id="main-wrapper">
+        <div className="site-wrapper-reveal">
+
+
+                
+                <div className="feature-large-images-wrapper infotechno-section-bg-01 section-space--pt_100">
+                    <div className="container">
+
+                        <div className="row">
+                            <div className="col-lg-12">
+                               
+                                <div className="section-title-wrap text-center section-space--mb_30">
+                                    <h6 className="section-sub-title mb-20">BLP EN DIGITAL</h6>
+                                    <h3 className="heading">LES NUITS DE CHALLENGE COVID-19</h3>
+                                </div>
+         
+                            </div>
+                        </div>
+
+                        <div className="row">
+                            <div className="col-12">
+                                <div className="row row--35">
+                      
+                        <div className="col-lg-4 col-md-6 mt-30">
+                                                        
+                             <div className="tab-history-image video-popup mt-30">
+                             <iframe  width="400" height="450" src="https://www.youtube.com/embed/G56wc5kl5gQ" frameborder="0" allowfullscreen autoplay="1"></iframe>
+                                     </div>
+                                         <div className="box-large-image__content mt-30 text-center">
+                                      <strong>Projet 01 : Application de reconnaissanceet de diagnostic de COVID19</strong> 
+                                </div>
+                            </div>
+          
+                           <div className="col-lg-4 col-md-6 mt-30">
+                                                        
+                                                        <div className="tab-history-image video-popup mt-30">
+                                 <iframe  width="400" height="450" src="https://www.youtube.com/embed/mIwKf4ojLL0" frameborder="0" allowfullscreen autoplay="1"></iframe>
+
+                                                                </div>
+                                                                    <div className="box-large-image__content mt-30 text-center">
+                                                                 <strong>Projet 2 : TBIBKOM</strong> 
+                                                           </div>
+                                                       </div>
+                        
+                        <div className="col-lg-4 col-md-6 mt-30">
+                                                        
+                            <div className="tab-history-image video-popup mt-30">
+                               <iframe  width="400" height="450" src="https://www.youtube.com/embed/dufzJAPW_VE" frameborder="0" allowfullscreen autoplay="1"></iframe>
+
+                               </div>
+                                 <div className="box-large-image__content mt-30 text-center">
+                                  <strong>Projet 5</strong> 
+                                   </div>
+                                     </div>
+              
+                            <div className="col-lg-4 col-md-6 mt-30">
+                                                        
+                                  <div className="tab-history-image video-popup mt-30">
+                                     <iframe  width="400" height="450" src="https://www.youtube.com/embed/xZcdUNMAWG4" frameborder="0" allowfullscreen autoplay="1"></iframe>
+                            
+                                        </div>
+                                             <div className="box-large-image__content mt-30 text-center">
+                                          <strong>Projet 6</strong> 
+                                  </div>
+                             </div>
+                      
+                            <div className="col-lg-4 col-md-6 mt-30">
+                                                        
+                                    <div className="tab-history-image video-popup mt-30">
+                                       <iframe  width="400" height="450" src="https://www.youtube.com/embed/Mi7i6DllDRI" frameborder="0" allowfullscreen autoplay="1"></iframe>
+                                                  
+                                    </div>
+                                        <div className="box-large-image__content mt-30 text-center">
+                                     <strong> Projet 7 : Prédiction des nombres d’infectés, des morts, de guérison</strong> 
+                                    </div>
+                             </div>
+       
+                               <div className="col-lg-4 col-md-6 mt-30">
+                                                        
+                                                        <div className="tab-history-image video-popup mt-30">
+                                                           <iframe  width="400" height="450" src="https://www.youtube.com/embed/mxNIshUpTQI" frameborder="0" allowfullscreen autoplay="1"></iframe>
+                                                                      
+                                                        </div>
+                                                            <div className="box-large-image__content mt-30 text-center">
+                                                         <strong> Projet 8</strong> 
+                                                        </div>
+                                                 </div>
+                      
+                           <div className="col-lg-4 col-md-6 mt-30">
+                                                        
+                                                        <div className="tab-history-image video-popup mt-30">
+                                                           <iframe  width="400" height="450" src="https://www.youtube.com/embed/_5R2vC-aCgE" frameborder="0" allowfullscreen autoplay="1"></iframe>
+                                                                      
+                                                        </div>
+                                                            <div className="box-large-image__content mt-30 text-center">
+                                                         <strong> Projet 9</strong> 
+                                                        </div>
+                                                 </div>
+
+                                </div>
+
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+  <div className="contact-us-section-wrappaer processing-contact-us-bg section-space--ptb_120">
+                <div className="container">
+                    <div className="row align-items-center">
+                        <div className="col-lg-6 col-lg-6">
+                        <div className="typed-text-wrap ">
+                                <h4 className="cd-headline letters type">
+                                    <span className=" text-white">Plus de </span>
+                                    <span className="cd-words-wrapper waiting">
+                            <b className="is-visible text-white">Spécialisation</b>
+                            <b className=" text-white">Professionnalisation</b>
+                            <b className=" text-white">Internationalisation</b>
+                            <b className=" text-white">Certification</b>
+                           </span> <span className=" text-white">   de chaque parcours.</span>
+                                </h4>
+                            </div>
+                        </div>
+
+                        <div className="col-lg-6 col-lg-6">
+                            <div className="contact-info-two text-center">
+                                <div className="icon">
+                                    <span className="fal fa-phone"></span>
+                                </div>
+                                <h6 className="heading font-weight--reguler">REJOIGNEZ MAINTENANT!!</h6>
+                                <h2 className="call-us"><a href="tel:190068668">(+216) 71 903 215</a></h2>
+                                <div className="contact-us-button mt-20">
+                                    <a href="contact" className="btn btn--secondary">Contactez-nous</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+ 
+        </div>
+</div>
 
         </>
     )
