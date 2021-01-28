dd60df3bfa5f010955f4a1703594c3fb3d5aca8f
AnessB <bouaoud.aness98@gmail.com>
2021-01-28 13:11:57 +0100

    edite document.js

--- pages/_document.js
+++ pages/_document.js
@@ -26,12 +26,12 @@ class MyDocument extends Document {
           <script src="assets/js/vendor/modernizr-2.8.3.min.js"></script>
           <script src="assets/js/vendor/jquery-3.5.1.min.js"></script>
           <script src="assets/js/vendor/jquery-migrate-3.3.0.min.js"></script>
+          <script src="assets/js/plugins/plugins.min.js"></script>
+          <script src="assets/js/main.js"></script>
 
           <script src="assets/js/vendor/bootstrap.min.js"></script>
 
-          <script src="assets/js/plugins/plugins.min.js"></script>
-
-          <script src="assets/js/main.js"></script>
+          
 
         </body>
       </Html>
