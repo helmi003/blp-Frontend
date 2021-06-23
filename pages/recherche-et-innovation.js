10a4ce2c1b08f60a44f486348c2e09d022d63f09
safa hamrouni <safahamrouni114@gmail.com>
2021-06-23 17:36:01 +0200

    recherche et inovation

---
+++ pages/recherche-et-innovation.js
@@ -0,0 +1,89 @@
+import React from 'react'
+
+export default function recherche() {
+    return (
+        <>
+
+<div className="breadcrumb-area">
+    <div className="container">
+        <div className="row">
+            <div className="col-12">
+                <div className="breadcrumb_box text-center">
+                    <h2 className="breadcrumb-title ">RECHERCHE & INNOVATION</h2>
+                
+                    <ul className="breadcrumb-list">
+                        <li className="breadcrumb-item"><a href="accueil">BLP en digital</a></li>
+                        <li className="breadcrumb-item active">Recherche & innovation</li>
+                    </ul>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="main-wrapper">
+    <div className="site-wrapper-reveal">
+        <div className="blog-pages-wrapper section-space--ptb_100">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-8 ml-auto mr-auto">
+                        <div className="main-blog-wrap">
+                            <div className="single-blog-item  wow move-up">                  
+                                <div className="post-info lg-blog-post-info">
+                                    <div className="post-categories text-center">
+                                        <a href="accueil"> BLP en digital </a>
+                                    </div>
+
+                                    <h3 className="post-title text-center">
+                                        <a href="recherche-et-innovation">Recherche & Innovation</a>
+                                    </h3>
+                                    <div className="post-excerpt mt-15">
+                                        <h6><b>L'École Supérieure des Sciences Juridiques, Economiques et Politiques de Tunis, BLP, est un établissement privé pluridisciplinaire visant les métiers du droit privé et public et droit des TIC et des affaires, de la finance, de la diplomatie et les montages juridiques et financiers.</b></h6>
+                                        <p>Avec son staff enseignant , elle est actuellement une parmi les grandes institutions de formation dans le secteur privé en Tunisie. En parallèle, et dans le cadre de son ouverture à l’international, BLP propose des parcours avec des doubles diplomes (1-2 ans en Tunisie, 1-2 ans à l'international)  avec des universités françaises; Suisse, Indiennes et canadiennes. </p>
+                                        <p><b>BLP </b>a réussi à mettre sur pied des formations de plus en plus en phase avec le monde du travail. Cette démarche lui a permis d’être mieux à l’écoute des besoins de ses partenaires économiques en matière de ressources humaines et de compétences dans les domaines juridico-politico-financiers.</p>
+                                        <p>Largement ouverte sur son entourage socio-économique, BLP n’a cessé d’œuvrer pour encourager le développement durable et être en harmonie avec les défis de la mondialisation. Le rapprochement entre l’université et le secteur économique national et international est désormais une réalité objective, un challenge, une stratégie de la BLP pour la mise en œuvre des projets prometteurs.</p>
+                               
+                                        <div className="single-blog-item lg-blog-item border-bottom wow move-up">
+                                  
+                                            <div className="post-feature post-quote">
+                                                <h5 className="post-quote-text text-center">
+                                                    “ La volonté d’ouverture BLP vers le monde  ”
+                                                </h5>
+                                            </div>
+                                   
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+      
+        <div className="cta-image-area_one section-space--ptb_80 cta-bg-image_one">
+            <div className="container">
+                <div className="row align-items-center">
+                    <div className="col-xl-8 col-lg-7">
+                        <div className="cta-content md-text-center">
+                            <h3 className="heading text-white">Double cursus: M1 en <span className="text-color-secondary">
+                                    Tunisie </span> M2 en <span className="text-color-secondary"> Europe</span> </h3>
+                        </div>
+                    </div>
+                    <div className="col-xl-4 col-lg-5">
+                        <div className="cta-button-group--one text-center">
+
+                            <a href="contact" className="btn btn--secondary  btn-two"><span className="btn-icon mr-2"><i
+                                        className="far fa-info-circle"></i></span>Contact</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    </div>
+        </>
+    )
+}
