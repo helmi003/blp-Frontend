1c6921f53e37f9aa744dc982a61abb58130860e9
haouet amani <haouetamani.ha@gmail.com>
2021-07-02 12:55:55 +0100

    inscription

--- pages/inscription.js
+++ pages/inscription.js
@@ -9,7 +9,7 @@ export default function inscription() {
             <div className="row">
                 <div className="col-12">
                     <div className="breadcrumb_box text-center">
-                        <h2 className="breadcrumb-title ">PRÉ-INSCRIPTION</h2>
+                        <h2 className="breadcrumb-title ">INSCRIPTION</h2>
                  
                     </div>
                 </div>
@@ -23,12 +23,66 @@ export default function inscription() {
                     <div className="row">
                         <div className="col-lg-12">
                             <div className="post-info lg-blog-post-info">
+                                <blockquote>
+                                            <p className="p1">S'inscrire à Tunisie : </p>
+                                </blockquote>
                                 <div className="post-excerpt mt-15">
-                                    <p style={{textAlign:"justify" , fontSize:"large"}}> Inscrivez-vous dès maintenant à BLP, une université qui accueille des étudiants de divers pays et offre un large choix de filières et de diplômes reconnus par l’Etat.<br/>
+                                    <p style={{textAlign:"justify" , fontSize:"large"}}> Inscrivez-vous à BLP, et obtenez un diplôme reconnu par l’Etat Tunisien.<br/>
                                                 Vous pouvez télécharger le dossier d'inscription en cliquant ici : <a href="assets/images/inscription/Dossier de candidature_alternatif-blp.docx">Dossier d'inscription</a><br/>
                                                 Inscrivez-vous dès maintenant sur le lien suivant : <a href="https://admission.blp.tn/"><u>Pré-inscription</u></a></p>
-                                </div><br/><br/>
-                            </div>
+                                </div>
+                                <blockquote>
+                                            <p className="p1">S'inscrire à l'international : </p>
+                                </blockquote>
+                                    <p style={{textAlign:"justify" , fontSize:"large"}}> Si vous souhaitez un double cursus qui débute en Tunisie et continue en Europe, nos partenaires vous offrent des opportunités.<br/>
+                                    <strong><p style={{color:"#d2a98e", fontSize:"large"}}>1. Cycle INGÉNIEUR Français à Ecole Polytech Saint Louis, ECAM-EPMI<br/></p></strong>
+                                   <p style={{textAlign:"justify", fontSize:"large"}}> Pour s’inscrire en Licence et Cycle INGÉNIEUR Français à Ecole Polytech Saint Louis, ECAM-EPMI (accrédité par la commission des titres d’ingénieurs en France), CTI :<br/>
+                                    Faire 1-2-3 ans en Tunisie<br/>
+                                    Faire 2 ans à ECAM-EPMI France : pour obtenir diplôme Ingénieur Français <br/>
+                                    <b>Niveau d’Admission :</b> Niveau Licence L1 et L2 et L3 : Toutes les spécialités techniques et informatiques<br/>
+                                    <u style={{color:"#007bff"}}>Contact en France :</u><br/>
+                                    <b> Nathalie ZARATÉ :</b> Responsable Service des Admissions<br/>
+                                    <b>Solenn DAUDU :</b> Directrice Relations Internationales<br/>
+                                    <b>Contact en International :</b> contactinternational@ecam-epmi.com<br/>
+                                    Veuillez trouver ci-après le lien vers le formulaire d'inscription Institut polytechnique saint Louis ECAM/EPMI pour la saison 2021/2022 :<br/>
+                                    <div className="btn-text">
+                                        <a href="https://docs.google.com/forms/d/e/1FAIpQLSfdX8IqKBevykAUdrKw5FcDvPJwHhALqBQqXDZEVfylFdBkGg/viewform"><i className="ml-1 button-icon far fa-long-arrow-right"></i> Formulaire d'inscription ECAM/EPMI 2021/2022 </a>
+                                    </div>
+                                    </p>
+                                    <strong><p style={{color:"#d2a98e", fontSize:"large"}}> 2. Pour s’inscrire en Master Européen en Management des risques à Cy Cergy Paris université<br/></p></strong>
+                                    <b>Niveau d’Admission :</b> Maîtrise ou M1 toutes spécialités en sciences de gestion<br/>
+                                    <b>Durée des Études :</b> 1 an en M2  ; M1 en Tunisie et M2 en Europe; soutenir mémoire a Paris<br/>
+
+                                    <u style={{color:"#007bff"}}>Contact en France :</u><br/>
+                                    <b>Jean luc Prigent :</b> jean-luc.prigent@cyu.fr<br/>
+                                    Vous pouvez télécharger le dossier d'inscription en cliquant ici :<br/>
+                                    <div className="btn-text">
+                                        <a href="assets/images/inscription/Dossier inscription M2 GRFI UMLT TUNISIE 2020-21.pdf"><i className="ml-1 button-icon far fa-long-arrow-right"></i> Dossier d'inscription à Cy Cergy Paris </a>
+                                    </div>
+                                    <br/>
+                                    <strong><p style={{color:"#d2a98e", fontSize:"large"}}>3. DBA, Doctorat en Business Administration ; France à Toulon<br/></p></strong>
+                                    <p style={{textAlign:"justify" , fontSize:"large"}}>Pour s’inscrire en DBA, Doctorat en Business Administration; en France à Toulon sans passer par notre ecole :<br/>
+                                    Carte d’étudiant française et diplôme français<br/>
+                                    <b>Niveau d’Admission : </b>Niveau Licence (3 ans d’expérience) ; Niveau M1 (2 ans expérience) et niveau M2, Toutes les spécialités<br/>
+                                    <b> Durée des Études :</b> 2 ans<br/>
+                                    Cours dispensé par les responsables français du DBA<br/>
+                                    <b> Inscription :</b> Doctorat en Business administration sur 2 ans<br/>
+
+                                    Soutenir en France<br/>
+
+                                    <b>Thèse : </b>rédiger thèse minimum 50 pages en Français ou en Anglais avec aide de professeurs à la bibliothèque<br/>
+                                    <u style={{color:"#007bff"}}>Contact en France :</u><br/>
+                                    <b>Aladdin Bijo :</b> bijo.a@me.com<br/>
+                                    Vous pouvez télécharger le dossier d'inscription en cliquant ici :<br/>
+                                    <div className="btn-text">
+                                        <a href="assets/images/inscription/Dossier inscription M2 GRFI UMLT TUNISIE 2020-21.pdf"><i className="ml-1 button-icon far fa-long-arrow-right"></i> Dossier d'inscription à HECCI </a>
+                                    </div>
+                                    </p>
+                                        
+                                                <br/>
+                                                </p>
+                                                </div>
+
                         </div>
                     </div>
                 </div>
